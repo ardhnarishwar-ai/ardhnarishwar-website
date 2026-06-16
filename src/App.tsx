394a826263dcb4a5bca16e90c019d6ae86e57abd
ardhnarishwar-ai <solution.funda@gmail.com>
2026-06-17 01:04:48 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,5 @@
-import { Timeline } from './components/sections/Timeline'
-import { Shakti } from './components/sections/Shakti'
-import { WhyWeExist } from './components/sections/WhyWeExist'
-import { KnowledgeCenter } from './components/sections/KnowledgeCenter'
-
-import { Header } from './components/layout/Header'
-import { Footer } from './components/layout/Footer'
-
-import { Hero } from './components/sections/Hero'
-import { About } from './components/sections/About'
-import { Services } from './components/sections/Services'
-import { Process } from './components/sections/Process'
-import { Testimonials } from './components/sections/Testimonials'
-
-import { Contact } from './components/sections/Contact'
-import { FAQ } from './components/sections/FAQ'
 function App() {
-  // useScrollMotion()
-
-  return (
-    <>
-      {/* <Seo /> */}
-      <Header />
-      <main>
-        <Hero />
-        <About />
-        <Services />
-        <Process />
-        <Testimonials />
-{/* <AboutSRaja /> */}
-<Timeline />
-<WhyWeExist />
-<Shakti />
-<KnowledgeCenter />
-<FAQ />
-<Contact />
-      </main>
-      <Footer />
-    </>
-  )
+  return <h1>App Working ✅</h1>
 }
 
 export default App
