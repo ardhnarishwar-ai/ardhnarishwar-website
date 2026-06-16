2bb067ddb76257b73df58309d281cb195375df8c
ardhnarishwar-ai <solution.funda@gmail.com>
2026-06-17 00:57:52 +0530

    Test main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,5 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HelmetProvider } from 'react-helmet-async'
-import './index.css'
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
-  </StrictMode>,
+  <h1>Hello Raja 🚀</h1>
 )
